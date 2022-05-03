a67dc27c5fe94d5e8bb5dd510b9e974a17e00a25
zhangchuanben <chuanben.zhang@exaleap.ai>
2022-05-03 11:26:21 +0800

    add scalafmt

--- build.scala
+++
@@ -1,8 +0,0 @@
-//> using scala "3.1.1"
-
-//> using options "-feature"
-//> using options "-deprecation"
-//> using options "-source:future"
-//> using options "-Ykind-projector:underscores"
-
-//> using lib "org.scalameta::munit:0.7.29"
